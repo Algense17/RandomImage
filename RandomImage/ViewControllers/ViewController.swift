de85ccafc76d0c99d162a684dacec06eef409e97
Algense17 <leonv909@gmail.com>
2025-04-02 23:27:51 +0300

    fix search

--- RandomImage/ViewControllers/ViewController.swift
+++ RandomImage/ViewControllers/ViewController.swift
@@ -7,40 +7,76 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
     
     @IBOutlet private var imageView: UIImageView!
-
+    @IBOutlet private var searchBar: UISearchBar!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         refreshButton()
         setupUnavailableConfig()
+        setupSearchBar()
         
     }
-
+    
     @IBAction func refreshButton() {
-        URLSession.shared.dataTask(with: URL(string: "https://picsum.photos/400/700")!) { data, response, error in
-                guard let data, let response else {
-                    print(error?.localizedDescription ?? "No error description")
-                    return
-                }
-                
-                print(response)
-                
-                DispatchQueue.main.async { [weak self] in
-                    guard let self else { return }
-                    imageView.image = UIImage(data: data)
-                    contentUnavailableConfiguration = nil
-                }
-            }.resume()
+        URLSession.shared.dataTask(with: URL(string: "https://picsum.photos/200/300")!) { data, response, error in
+            guard let data, let response else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            print(response)
+            
+            DispatchQueue.main.async { [weak self] in
+                guard let self else { return }
+                imageView.image = UIImage(data: data)
+                contentUnavailableConfiguration = nil
+            }
+        }.resume()
+    }
+    
+    private func setupUnavailableConfig() {
+        var config = UIContentUnavailableConfiguration.loading()
+        config.text = "Waiting for a response..."
+        config.textProperties.color = .systemBlue
+        config.textProperties.font = .boldSystemFont(ofSize: 20)
+        contentUnavailableConfiguration = config
+    }
+    
+    internal func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        guard let searchText = searchBar.text, !searchText.isEmpty else {
+            return
         }
-
-        private func setupUnavailableConfig() {
-            var config = UIContentUnavailableConfiguration.loading()
-            config.text = "Waiting for a response..."
-            config.textProperties.color = .systemBlue
-            config.textProperties.font = .boldSystemFont(ofSize: 20)
-            contentUnavailableConfiguration = config
+        
+        let urlString = "https://picsum.photos/search?query=\(searchText)"
+        guard let url = URL(string: urlString) else {
+            return
         }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data, let response else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            print(response)
+            
+            DispatchQueue.main.async { [weak self] in
+                guard let self else { return }
+                imageView.image = UIImage(data: data)
+                contentUnavailableConfiguration = nil
+            }
+        }.resume()
+        
+        searchBar.resignFirstResponder()
+    }
+    
+    private func setupSearchBar() {
+        searchBar.delegate = self
+        searchBar.placeholder = "Search images..."
+    }
 }
 
